4f30cee3809c2b1effe789fca21ccfb768ead945
lf- <lf-@users.noreply.github.com>
2020-09-06 02:07:55 -0700

    Fix extended frames

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,61 @@
 #![recursion_limit = "1024"]
-extern crate byteorder;
-extern crate clap;
 #[macro_use]
 extern crate error_chain;
-extern crate pcap;
 
 mod errors {
-    error_chain!{}
+    error_chain! {}
 }
-use errors::*;
+use byteorder::{LittleEndian, ReadBytesExt};
 use clap::{App, Arg};
+use errors::*;
 use pcap::Capture;
 use std::io::Cursor;
-use byteorder::{LittleEndian, ReadBytesExt};
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const AUTHOR: &'static str = concat!("by ", env!("CARGO_PKG_AUTHORS"));
+
+const EXTENDED_FLAG_MASK: u32 = 1 << 31;
+const ID_PART_MASK: u32 = (1 << 29) - 1;
+
+type CanId = u32;
+
+#[derive(Debug, PartialEq, Eq)]
+struct CanFrame {
+    is_extended: bool,
+    id: CanId,
+    datalen: u8,
+    data: [u8; 8],
+}
+
+impl CanFrame {
+    // https://github.com/torvalds/linux/blob/3926a3a/include/uapi/linux/can.h#L93
+    /// Decodes a CAN frame
+    fn new(frame: &[u8]) -> Option<CanFrame> {
+        let mut reader = Cursor::new(frame);
+        let id_ = reader.read_u32::<LittleEndian>().ok()?;
+        let id = id_ & ID_PART_MASK;
+        let is_extended = ((id_ & EXTENDED_FLAG_MASK) >> 31) == 1;
+        let datalen = reader.read_u8().ok()?;
+
+        // skip reserved bytes
+        reader.set_position(reader.position() + 3);
+        if datalen > 8 {
+            return None;
+        }
+
+        let mut dataout = [0u8; 8];
+
+        for i in 0..datalen as usize {
+            dataout[i] = reader.read_u8().ok()?;
+        }
+        Some(CanFrame {
+            is_extended,
+            id,
+            datalen,
+            data: dataout,
+        })
+    }
+}
 
 fn run() -> Result<()> {
     // manage command line arguments using clap
@@ -29,47 +69,63 @@ fn run() -> Result<()> {
                 .help("the pcap file to convert")
                 .required(true),
         )
+        .arg(
+            Arg::with_name("devname")
+                .long("devname")
+                .help("device name to put in the log")
+                .required(false)
+                .default_value("can0"),
+        )
         .get_matches();
 
     // load pcap file
-    let mut cap =
-        Capture::from_file(matches
+    let mut cap = Capture::from_file(
+        matches
             .value_of("pcap_file")
-            .chain_err(|| "invalid pcap file name")?)
-            .chain_err(|| "could not load pcap file")?;
+            .chain_err(|| "invalid pcap file name")?,
+    )
+    .chain_err(|| "could not load pcap file")?;
+
+    let devname = matches
+        .value_of("devname")
+        .chain_err(|| "invalid devname")?;
 
     while let Ok(packet) = cap.next() {
         // slice protocol from packet
         let protocol = &packet.data[14..16];
         // check if it is a can packet, if not ignore it
-        if protocol == [0, 0xc] {
-            // split packet data from packet metadata
-            let packet_data = &packet.data[16..];
-            // create byte reader to convert multiple u8 with correct endianess
-            let mut rdr = Cursor::new(packet_data);
-            // extract can id
-            let can_id = rdr.read_u16::<LittleEndian>()
-                .chain_err(|| "could not read can id from packet")?;
-            // extract length of can data
-            let can_data_len = packet_data[4] as usize;
-            // extract can data
-            if packet_data.len() < can_data_len + 8 {
-                eprintln!("malformed can packet, data is shorter than given length");
-                continue;
-            }
-            let can_msg = &packet_data[8..can_data_len + 8];
-            // convert can data to hex string
-            let mut can_hex_str = String::new();
-            for b in can_msg.iter() {
-                can_hex_str.push_str(format!("{:02X}", b).as_ref());
-            }
-            // print the packet in canlog format
-            // one might what to change the device name used, but thats future work :D
-            println!(
-                "({}.{:06}) can0 {:X}#{}",
-                packet.header.ts.tv_sec, packet.header.ts.tv_usec, can_id, can_hex_str
-            );
+        if protocol != [0, 0xc] {
+            continue;
+        }
+
+        // split packet data from packet metadata
+        let packet_data = &packet.data[16..];
+        let frame = CanFrame::new(packet_data);
+        let frame = match frame {
+            Some(f) => f,
+            None => continue,
         };
+
+        // convert can data to hex string
+        let mut can_hex_str = String::new();
+        for b in frame.data[0..frame.datalen as usize].iter() {
+            can_hex_str.push_str(format!("{:02X}", b).as_ref());
+        }
+
+        // print the packet in canlog format
+        match frame.is_extended {
+            // candump help:
+            // "When the can_id is 8 digits long the CAN_EFF_FLAG is set for 29 bit EFF format."
+            true => println!(
+                "({}.{:06}) {} {:08X}#{}",
+                packet.header.ts.tv_sec, packet.header.ts.tv_usec, devname, frame.id, can_hex_str
+            ),
+            // otherwise it is 3 digits long
+            false => println!(
+                "({}.{:06}) {} {:03X}#{}",
+                packet.header.ts.tv_sec, packet.header.ts.tv_usec, devname, frame.id, can_hex_str
+            ),
+        }
     }
 
     Ok(())
@@ -95,5 +151,37 @@ fn main() {
         }
 
         ::std::process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_decoding() {
+        // only actual payload
+        let ext1 = b"\x48\x20\x00\x80\x02\x00\xad\xde\x01\x30\x00\x00\x00\x00\xad\xde";
+
+        assert_eq!(
+            CanFrame::new(ext1),
+            Some(CanFrame {
+                is_extended: true,
+                id: 0x2048,
+                datalen: 2,
+                data: [0x01, 0x30, 0, 0, 0, 0, 0, 0],
+            })
+        );
+
+        let bas1 = b"\x99\x07\x00\x00\x02\x10\x00\x00\x01\x31\x36\x2e\x35\x00\x00\x00";
+        assert_eq!(
+            CanFrame::new(bas1),
+            Some(CanFrame {
+                is_extended: false,
+                id: 0x799,
+                datalen: 2,
+                data: [0x01, 0x31, 0, 0, 0, 0, 0, 0],
+            })
+        );
     }
 }
